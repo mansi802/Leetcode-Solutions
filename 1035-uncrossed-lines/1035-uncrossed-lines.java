fc3ff933bd5bf4ff453311b0fdf433a4a50ab1c7
Mansi <86536299+mansi802@users.noreply.github.com>
2023-05-11 12:13:41 +0530

    Time: 5 ms (92.56%), Space: 42.2 MB (67.74%) - LeetHub

---
+++ 1035-uncrossed-lines/1035-uncrossed-lines.java
@@ -0,0 +1,27 @@
+class Solution {
+    //longest common subsequnce
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int n=nums1.length;
+        int m=nums2.length;
+        
+        int[][] memo=new int[n+1][m+1];
+        
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(nums1[i-1]==nums2[j-1]) memo[i][j]=1+memo[i-1][j-1];
+                else memo[i][j]=Math.max(memo[i-1][j],memo[i][j-1]);
+            }
+        }
+        
+        return memo[n][m];
+        // return helper(nums1,nums2,0,0);
+    }
+    
+    private int helper(int[] nums1,int[] nums2,int i,int j){
+        if(i==nums1.length || j==nums2.length) return 0;
+        
+        if(nums1[i]==nums2[j]) return 1+helper(nums1,nums2,i+1,j+1);
+        
+        return Math.max(helper(nums1,nums2,i+1,j),helper(nums1,nums2,i,j+1));
+    }
+}
